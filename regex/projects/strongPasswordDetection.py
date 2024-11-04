9e7143fff05a0d864e3b38b6f6cbf4ed650dd91b
Tavish <tavilefty666@gmail.com>
2024-11-04 08:57:19 +0530

    Used GetPass to hide input

--- regex/projects/strongPasswordDetection.py
+++ regex/projects/strongPasswordDetection.py
@@ -1,12 +1,18 @@
 import re
+from getpass4 import getpass
 
 def is_strong_password(password):
+    # m3gakr4nus called it 'regex on god mode' and I agree.
+    ''' The (?=.*[<some-pattern>]) syntax means that pattern should be present
+    and the next such syntax also must meet, altho not in the same order.
+    The pattern expects small alphabet, big alphabet, number, any symbol from the set
+    ALSO it should be 8 or more characters as defined in the last one'''
     pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[._@#!$%&*])[a-zA-Z0-9._@#!$%&*]{8,}$'
     return bool(re.search(pattern, password))
 
-password = input('Enter your password: ')
+password = getpass('Enter your password: ')
 
 if is_strong_password(password):
     print('Stronk')
 else:
-    print('Weak')
+    print('Weakling')
